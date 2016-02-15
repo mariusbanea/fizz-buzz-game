07712f04ffe16196f0e9d1a675edb0c2940c674b
Marius Banea <marius@w-d-s.co.uk>
2016-02-15 05:06:18 +0000

    creating the validation function

--- js/fizzbuzz-refactor.js
+++ js/fizzbuzz-refactor.js
@@ -10,6 +10,44 @@
 First declare the functions
 ****************************/
 
+//PART 2 => continuing the validation
+function validate(inputNumber) {
+
+    //by default we are assuming that the input is valid (setting a FLAG to true)
+    var userInputCorrectFlag = true;
+
+    //at least 3 characters
+    while (inputNumber.length < 2) {
+        var inputNumber = prompt("Please enter 2 digits or more.");
+        userInputCorrectFlag = false;
+    }
+    //no spaces
+    while (inputNumber.indexOf(' ') >= 0) {
+        var inputNumber = prompt("Please don't enter spaces. Try a number!");
+        userInputCorrectFlag = false;
+    }
+    //the limit is a number (if the rounded value of your number is the same with the initial number, it means that the number was integer in the first place)
+    while (Math.floor(inputNumber) != inputNumber) {
+        var inputNumber = prompt("Your upper limit was not a number. Set it again.");
+        //change the user input flag to false
+        userInputCorrectFlag = false;
+    }
+
+    //PART 3 => if the limit is a number, then play the game
+    if (userInputCorrectFlag = true) {
+        fizzbuzz(inputNumber);
+    }
+}
+
+//PART 3.1 => fizzbuz function is counting
+function fizzbuzz(limit) {
+
+    //loop through all the numbers and call the check counter function
+    for (var counter = 1; counter <= limit; counter++) {
+        $('#showResults').append(check(counter));
+    }
+}
+
 //PART 3.2 => check the each number and show the results
 function check(currentNumber) {
 
@@ -29,15 +67,6 @@ function check(currentNumber) {
     return msg;
 }
 
-//PART 3.1 => fizzbuz function is counting
-function fizzbuzz(limit) {
-
-    //loop through all the numbers and call the check counter function
-    for (var counter = 1; counter <= limit; counter++) {
-        $('#showResults').append(check(counter));
-    }
-}
-
 /****************************
 Then use the functions
 ****************************/
@@ -47,34 +76,7 @@ $(document).ready(function () {
     //PART 1 => get the upper limit from the user input
     var limit = prompt("Please set the upper limit to play FizzBuzz.");
 
-
-    /****************************
-    PART 2 => Starting the validation
-    ****************************/
-
-    //by default we are assuming that the input is valid (setting a FLAG to true)
-    var userInputCorrectFlag = true;
-
-    //at least 3 characters
-    while (limit.length < 2) {
-        var limit = prompt("Please enter 2 digits.");
-        userInputCorrectFlag = false;
-    }
-    //no spaces
-    while (limit.indexOf(' ') >= 0) {
-        var limit = prompt("Please don't enter spaces - try a number!");
-        userInputCorrectFlag = false;
-    }
-    //the limit is a number (if the rounded value of your number is the same with the initial number it means that the number was integer in the first place)
-    while (Math.floor(limit) != limit) {
-        var limit = prompt("Your upper limit was not correct. Set it again.");
-        //change the user input flag to false
-        userInputCorrectFlag = false;
-    }
-
-    //PART 3 => if the limit is a number, then play the game
-    if (userInputCorrectFlag = true) {
-        fizzbuzz(limit);
-    }
+    //PART 2 => Starting the validation
+    validate(limit);
 
 });
